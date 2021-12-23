5acf053ddedeb6507827505ef138088541f561ce
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2021-12-23 16:02:42 -0400

    UPDATE: release name update

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -2,12 +2,13 @@ module Api
     module V1
         class TestsController < ApplicationController
             def index
-                resp = HTTP.get('https://api.github.com/repos/rails/rails/releases').body
+                @apiRelease = 'https://api.github.com/repos/rails/rails/releases'
+                resp = HTTP.get(@apiRelease).body
                 releases = []
                 json_data = JSON.parse(resp)
                 json_data.each do |release|
-                    name = release['name']
-                    releases << name
+                    value = { release:  release['name']}
+                    releases << value
                 end
                 render json: releases
             end
